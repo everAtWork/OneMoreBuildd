44eff26e64dd4ccdb3331dd60b55878adfb35c1e
Ladislav Kožarin <59497788+everAtWork@users.noreply.github.com>
2020-09-30 05:55:21 +0300

    the build version @prodd

---
+++ static/js/main2jdlj.js
@@ -0,0 +1,11 @@
+// import * as $ from 'jquery';
+// import 'bootstrap';
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
